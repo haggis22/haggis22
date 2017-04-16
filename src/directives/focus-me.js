a1d5611a7e9c0831022dda4f5381f83a2e8a2925
haggis22 <dshell@gmail.com>
2017-04-15 22:32:23 -0500

    fixed attribute name to use h22 prefix

--- src/directives/focus-me.js
+++ src/directives/focus-me.js
@@ -5,12 +5,12 @@
     mod.directive('h22FocusMe', ['$timeout', function ($timeout) {
         return {
             link: function (scope, element, attrs) {
-                scope.$watch(attrs.focusMe, function (value) {
-                    if (value === true) {
+                scope.$watch(attrs.h22FocusMe, function (value) {
+                    if (value) {
                         // the timeout will give any container time to appear
                         $timeout(function () {
                             element[0].focus();
-                        }, 200);
+                        }, 10);
                     }
                 });
             }
